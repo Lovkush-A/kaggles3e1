b5af1825f5b078395b233dc2926fab38a42fc9cb
Lovkush Agarwal <lovkush@gmail.com>
2023-03-02 19:35:52 +0000

    shorten description in setup, add ds packages to requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Ronak and Lovkush learning Data Science. First project is to try Kaggle Season 3 Episode 1.',
+    description='Kaggle Season 3 Episode 1.',
     author='lovkush-ronak',
     license='MIT',
 )
